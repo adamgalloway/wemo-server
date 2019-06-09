8ae6ccba97f2d232de1e015d2ac1851f71556686
adamgalloway <adam.galloway@gmail.com>
2019-06-09 22:00:08 +0000

    update logging

--- index.js
+++ index.js
@@ -12,18 +12,22 @@ Object.keys(devices).forEach(deviceName => {
   var wemo = wemore.Emulate({ friendlyName: deviceName, port: port, uuid: device.uuid, serial: device.serial });
 
   wemo.on('listening', () => {
-    console.log(deviceName + " listening on port " + port);
+    console.log(deviceName, "listening on port", port, new Date());
+  });
+
+  wemo.on('state', function(binaryState, self, sender) {
+    console.log(deviceName, "set to", binaryState);
   });
 
   wemo.on('on', (self, sender) => {
     var command = device.oncommand;
-    console.log(deviceName + " on");
+    console.log(deviceName, "on", new Date());
     exec(command, (error) => { if (error) console.log(error); });				
   });
 
   wemo.on('off', (self, sender) => {
     var command = device.offcommand;
-    console.log(deviceName + " off");
+    console.log(deviceName, "off", new Date());
     exec(command, (error) => { if (error) console.log(error); });
   });
 });
